476bfd91c5a1bc7e8b3e78d2d08f22b090daba44
CaioBertolini <caio.bertolini58@gmail.com>
2021-04-30 18:46:49 -0300

    Adicionando o gh-pages e criação do modal pay

---
+++ src/components/pay.jsx
@@ -0,0 +1,24 @@
+import React  from 'react'
+
+// let cards = [
+//     // valid card
+//     {
+//       card_number: '1111111111111111',
+//       cvv: 789,
+//       expiry_date: '01/18',
+//     },
+//     // invalid card
+//     {
+//       card_number: '4111111111111234',
+//       cvv: 123,
+//       expiry_date: '01/20',
+//     },
+//   ];
+
+export default function Pay(){
+    return(
+        <div className="modal-showing">
+            <span>Teste</span>
+        </div>
+    )
+}
